f98002a76f0bef0e98cf907bb53bae6e0225f085
Nicolas Wagner <nicolas_wagner@gmx.de>
2020-07-07 15:35:05 +0200

    Deleted deep copy

--- training.py
+++ training.py
@@ -8,7 +8,7 @@ from kaggle_environments import evaluate, make, utils
 from agents import Q_Network
 import matplotlib.pyplot as plt
 import matplotlib
-import copy
+
 matplotlib.use('Agg') 
 
 class NetworkTrainer():
@@ -103,7 +103,7 @@ class NetworkTrainer():
                 self.module.eval()
                 if observation.mark != 1:
                     observation['board'] = switch(observation['board'])
-                observation_old = copy.deepcopy(observation)
+                observation_old = observation
                 if random.uniform(0, 1) <= self.eps:
                     action = choice([c for c in range(self.columns) if observation['board'][c] == 0])
                 else:

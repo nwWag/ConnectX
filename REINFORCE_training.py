f2dc3fc79d790d9fde4539ed2aaf8b8672155382
marco rettig <marco-r53@web.de>
2020-07-21 11:10:40 +0200

    Updated REINFORCE_training.py

--- REINFORCE_training.py
+++ REINFORCE_training.py
@@ -1,28 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import sys
-import torch
-import gym
 import numpy as np
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.autograd import Variable
 import matplotlib.pyplot as plt
 from kaggle_environments import evaluate, make, utils
 from tqdm import tqdm
-import copy
-from REINFORCE_Agent import PolicyNetwork
+from agents import PolicyNetwork
 
 
 # training:
 def train():
-    env = make("connectx", configuration= {'rows': 6, 'columns': 8, 'inarow': 4}, debug=True)
+    env = make("connectx", debug=False)
     trainer = env.train([None, "random"])
     
     net1 = PolicyNetwork(env.configuration.columns*env.configuration.rows, env.configuration.columns, 128)
-    #net1.load_state_dict(torch.load("./params.pth"))
+    #net1.load_state_dict(torch.load("model/REINFORCE_params.pth"))
         
     max_episode_num = 5000
     max_steps = 10000
@@ -52,7 +44,7 @@ def train():
             state = new_state.board
     
     # save model parameters
-    torch.save(net1.state_dict(), "./params.pth")
+    #torch.save(net1.state_dict(), "model/REINFORCE_params_old.pth")
         
     plt.plot(np.cumsum(all_rewards), label="Cumulated Reward")
     plt.xlabel('Episode')
